1c77c268dd947ad24c8fe9429c6c77cc4f161698
Petrosz007 <andipeti2000@gmail.com>
2021-01-06 14:45:50 +0100

    Restructured project and added unit tests

---
+++ GoblineerNextUpdater.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using GoblineerNextUpdater.Services;
+
+namespace GoblineerNextUpdater.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
