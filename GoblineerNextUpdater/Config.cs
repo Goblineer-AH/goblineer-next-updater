1c634147aa8d15bb830332575ebc387f598c35f6
Petrosz007 <andipeti2000@gmail.com>
2021-01-16 11:30:43 +0100

    Updated config loading

--- GoblineerNextUpdater/Config.cs
+++ GoblineerNextUpdater/Config.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
+using System.Reflection;
 using System.Text.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace GoblineerNextUpdater
 {
@@ -13,12 +15,29 @@ namespace GoblineerNextUpdater
     {
         public static Config LoadConfig(string filePath)
         {
-            var data = File.ReadAllText(filePath);
-            var config = JsonSerializer.Deserialize<Config>(data);
-            if(config is null)
-                throw new Exception("Config file is null.");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
 
-            return config;
+            var blizzardClientId = configuration.GetValue<string>("BLIZZARD_CLIENT_ID");
+            var blizzardClientSecret = configuration.GetValue<string>("BLIZZARD_CLIENT_SECRET");
+            var blizzardOAuthRegion = configuration.GetValue<string>("BLIZZARD_OAUTH_REGION");
+            var connectionString = ReadConnectionString(configuration);
+
+            return new(blizzardClientId, blizzardClientSecret, blizzardOAuthRegion, connectionString);
+        }
+
+        private static string ReadConnectionString(IConfiguration configuration)
+        {
+            var host     = configuration.GetValue<string>("DB_HOST");
+            var username = configuration.GetValue<string>("DB_USERNAME");
+            var password = configuration.GetValue<string>("DB_PASSWORD");
+            var database = configuration.GetValue<string>("DB_DATABASE");
+
+            var connString = $"Host={host};Username={username};Password={password};Database={database};Pooling=true;";
+
+            return connString;
         }
     }
 }
